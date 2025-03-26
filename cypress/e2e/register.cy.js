15b75b1e8dd11c4761a5d4bf32674533f7f28a4f
vascsx <vasczn@gmail.com>
2025-03-25 22:20:42 -0300

    feat: adicionando cenários com campos vazios do endpoint register

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,15 +1,35 @@
 describe('Testes no endpoint de registro de usuário', () => {
   
-  // Observação depois validar melhor este cenário
-  it('Cenário em que todos os campos estão vazios', () => {
+  // // Observação depois validar melhor este cenário
+  // it('Cenário em que todos os campos estão vazios', () => {
+
+  //   // Campos vazios
+  //   const newUser = {
+  //     "fullName": "andersonv asconcelos",
+  //     "email": "teste@gmail.com",
+  //     "password": "asasasas"
+  //   };
+
+  //   // Realizando a requisição
+  //   cy.request({
+  //     method: 'POST',
+  //     url: Cypress.env('apiRegisterUrl'), 
+  //     body: newUser, 
+  //     failOnStatusCode: false,
+  //   }).then((response) => {
+  //     expect(response.status).to.eq(400);
+  //   });
+  // });
+
+  it.only('Cenário em que fullName está vazio', () => {
 
     // Campos vazios
     const newUser = {
-      fullname: '',
-      email: '',
-      password: ''
+      "fullName": "",
+      "email": "teste@gmail.com",
+      "password": "testees"
     };
-
+  
     // Realizando a requisição
     cy.request({
       method: 'POST',
@@ -18,12 +38,53 @@ describe('Testes no endpoint de registro de usuário', () => {
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
+      expect(response.body.erro).to.equal("O campo 'FullName' é obrigatório.");
     });
   });
+  
+
+  it.only('Cenário em que email está vazio', () => {
+
+    // Campos vazios
+    const newUser = {
+      "fullName": "Anderson Vasc",
+      "email": "",
+      "password": "testees"
+    };
+  
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erro).to.equal("O campo 'Email' é obrigatório.");
+    });
+  });
+
+
+  it.only('Cenário em que password está vazio', () => {
+
+    // Campos vazios
+    const newUser = {
+      "fullName": "Anderson Vasc",
+      "email": "teste@gmail.com",
+      "password": ""
+    };
+  
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erro).to.equal("O campo 'Password' é obrigatório.");
+    });
+  });
+
+
 });
-
-
-  
-  
-  
-

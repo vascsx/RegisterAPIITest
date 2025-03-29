a727c08f47371bcdea674134dc59c9cac7608c9b
vascsx <vasczn@gmail.com>
2025-03-28 21:58:36 -0300

    feat: adição de cenários no endpoint de registro onde os campos do request está com formato incorreto

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,29 +1,96 @@
 describe('Testes no endpoint de registro de usuário', () => {
+
+  //Cenários em que os campos estão com formato incorreto
+
+  it('Cenário em que fullName está sendo preenchido com números', () => {
+
+    // Campo fullName vazio
+    const newUser = {
+      "fullName": "Anderson1",
+      "email": "teste@gmail.com",
+      "password": "senhaessss"
+    };
   
-  // // Observação depois validar melhor este cenário
-  // it('Cenário em que todos os campos estão vazios', () => {
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erros[0]).to.equal("O campo 'FullName' deve conter apenas letras");
+    });
+  });
 
-  //   // Campos vazios
-  //   const newUser = {
-  //     "fullName": "andersonv asconcelos",
-  //     "email": "teste@gmail.com",
-  //     "password": "asasasas"
-  //   };
+  it('Cenário em que email está sendo preenchido sem o @', () => {
 
-  //   // Realizando a requisição
-  //   cy.request({
-  //     method: 'POST',
-  //     url: Cypress.env('apiRegisterUrl'), 
-  //     body: newUser, 
-  //     failOnStatusCode: false,
-  //   }).then((response) => {
-  //     expect(response.status).to.eq(400);
-  //   });
-  // });
+    // Campo fullName vazio
+    const newUser = {
+      "fullName": "Anderson",
+      "email": "testegmail.com",
+      "password": "testeyes"
+    };
+  
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erros[0]).to.equal("O campo 'Email' não é válido.");
+    });
+  });
+
+  it('Cenário em que email está sendo preenchido sem o .com', () => {
+
+    // Campo fullName vazio
+    const newUser = {
+      "fullName": "Anderson",
+      "email": "teste@gmailcom",
+      "password": "testeees"
+    };
+  
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erros[0]).to.equal("O campo 'Email' não é válido.");
+    });
+  });
+
+  it('Cenário em que o password é menor que 6 caracteres', () => {
+
+    // Campo fullName vazio
+    const newUser = {
+      "fullName": "Anderson",
+      "email": "teste@gmail.com",
+      "password": "teese"
+    };
+  
+    // Realizando a requisição
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('apiRegisterUrl'), 
+      body: newUser, 
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.erros[0]).to.equal("A senha deve ter no mínimo 6 caracteres.");
+    });
+  });
+
+  //Cenários em que os campos estão vazios
 
   it('Cenário em que fullName está vazio', () => {
 
-    // Campos vazios
+    // Campo fullName vazio
     const newUser = {
       "fullName": "",
       "email": "teste@gmail.com",
@@ -38,14 +105,14 @@ describe('Testes no endpoint de registro de usuário', () => {
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
-      expect(response.body.erro).to.equal("O campo 'FullName' é obrigatório.");
+      expect(response.body.erros[0]).to.equal("O campo 'FullName' é obrigatório.");
     });
   });
   
 
   it('Cenário em que email está vazio', () => {
 
-    // Campos vazios
+    // Campo email vazio
     const newUser = {
       "fullName": "Anderson Vasc",
       "email": "",
@@ -60,14 +127,14 @@ describe('Testes no endpoint de registro de usuário', () => {
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
-      expect(response.body.erro).to.equal("O campo 'Email' é obrigatório.");
+      expect(response.body.erros[0]).to.equal("O campo 'Email' é obrigatório.");
     });
   });
 
 
   it('Cenário em que password está vazio', () => {
 
-    // Campos vazios
+    // Campo password
     const newUser = {
       "fullName": "Anderson Vasc",
       "email": "teste@gmail.com",
@@ -82,7 +149,7 @@ describe('Testes no endpoint de registro de usuário', () => {
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
-      expect(response.body.erro).to.equal("O campo 'Password' é obrigatório.");
+      expect(response.body.erros[0]).to.equal("O campo 'Password' é obrigatório.");
     });
   });
 
@@ -103,7 +170,7 @@ describe('Testes no endpoint de registro de usuário', () => {
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
-      expect(response.body.erro).to.equal("Nenhum campo foi preenchido!.");
+      expect(response.body.erro).to.equal("Nenhum campo foi preenchido!");
     });
   });
 
